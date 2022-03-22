19c6a629c50059ce03ed400d4da9d66b22c07d6b
Jergus Kacmar <jergus.kacmar@student.tuke.sk>
2022-03-22 18:28:08 +0100

    chore(contact-page): update link to github

--- src/app/utils/hrefs.ts
+++ src/app/utils/hrefs.ts
@@ -1,6 +1,6 @@
 export const facebook = 'https://www.facebook.com/jergus.kacmar/';
 export const instagram = 'https://www.instagram.com/xdexerio/';
-export const github = 'https://github.com/Dexerius/';
+export const github = 'https://github.com/JergusKacmar/';
 
 const redirect = (path: string): void => {
   window.open(path, '_blank');
